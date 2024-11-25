7526700fe857994d918c3c67bcae85fd8656c809
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2024-11-25 14:09:53 +0200

    Update sqltest.php

--- src/sqltest.php
+++ src/sqltest.php
@@ -12,9 +12,33 @@
     }
     else {
         echo"Connection Success: connected!";
-
     }
-    
+
+    $tsql= "SELECT Maakunta_Id, Teema, Uutinen, Url FROM dbo.Mediaseuranta;";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        die(FormatErrors(sqlsrv_errors()));
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo ($row['Maakunta_ID'] . " " . $row['Teema'] . " " . $row['Uutinen'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+
+
+    function FormatErrors( $errors )
+{
+    /* Display errors. */
+    echo "Error information: ";
+
+    foreach ( $errors as $error )
+    {
+        echo "SQLSTATE: ".$error['SQLSTATE']."";
+        echo "Code: ".$error['code']."";
+        echo "Message: ".$error['message']."";
+    }
+}
+
+/*
     $tsql= "SELECT TOP 20 Uuutinen as Uutinen, Teema as Teema
          FROM Mediaseuranta";
     $getResults= sqlsrv_query($conn, $tsql);
@@ -25,4 +49,6 @@
      echo ($row['Uutinen'] . " " . $row['Teema'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
+    */
+    
 ?>
