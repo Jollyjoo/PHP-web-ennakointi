cae71cb1af18cf951669c518b2007c0826b8226d
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2024-11-27 15:47:42 +0200

    Update haehakusanalla.php

--- src/haehakusanalla.php
+++ src/haehakusanalla.php
@@ -35,7 +35,7 @@
         $url = htmlspecialchars_decode($url, ENT_QUOTES);
         // Remove non-breaking spaces and other unwanted characters
         $url = str_replace(array('%C2%A0', '%20', ' '), '', $url);
-        echo ("<b>" . $row['aika'] . " " . $row['Teema'] . "</b> " . $row['Uutinen'] . " <a href='" . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . "'>Linkki</a><br>" . PHP_EOL);
+        echo ("<b>test " . $row['aika'] . " " . $row['Teema'] . "</b> " . $row['Uutinen'] . " <a href='" . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . "'>Linkki</a><br>" . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
     
