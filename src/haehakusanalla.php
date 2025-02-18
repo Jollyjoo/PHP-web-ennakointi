6192f279f860e8dd632f7821ea03c393ceb8c976
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2025-02-18 14:03:35 +0200

    Update haehakusanalla.php

--- src/haehakusanalla.php
+++ src/haehakusanalla.php
@@ -16,10 +16,17 @@ if ($conn->connect_error) {
 
 $q = $_GET['q'];
 
-$sql = "SELECT CONVERT(CHAR(8),[uutisen_pvm],112) as aika, Maakunta_ID, Teema, Uutinen, Url 
+/* $sql = "SELECT CONVERT(CHAR(8),[uutisen_pvm],112) as aika, Maakunta_ID, Teema, Uutinen, Url 
                 FROM Mediaseuranta
                 where Uutinen LIKE '%" . $q . "%'
+                order by uutisen_pvm DESC;"; */
+
+            $sql = "SELECT uutisen_pvm as aika, Maakunta_ID, Teema, Uutinen, Url 
+                FROM catbxjbt_ennakointi.Mediaseuranta
+                where Uutinen LIKE '%" . $q . "%'
                 order by uutisen_pvm DESC;";
+                
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
