7a60c086b535a6c4f7df5e428d34ec34008ce3ab
Jollyjoo <156919086+Jollyjoo@users.noreply.github.com>
2025-04-10 15:47:53 +0300

    lots

--- src/haeMaakunnalla.php
+++ src/haeMaakunnalla.php
@@ -20,13 +20,13 @@ $start = isset($_GET['start']) ? intval($_GET['start']) : 0; // Default to 0 if 
 // Adjust the query based on the value of 'q'
 if ($q === "koko-häme") {
     // Fetch all content if 'koko-häme' is selected
-    $sql = "SELECT uutisen_pvm as aika, Maakunta_ID, Teema, Uutinen, Url 
+    $sql = "SELECT uutisen_pvm as aika, Maakunta_ID, Teema, Uutinen, Hankkeen_luokitus, Url 
             FROM catbxjbt_ennakointi.Mediaseuranta
             ORDER BY uutisen_pvm DESC
             LIMIT $start, 20;";
 } else {
     // Fetch content filtered by 'Maakunta_ID'
-    $sql = "SELECT uutisen_pvm as aika, Maakunta_ID, Teema, Uutinen, Url 
+    $sql = "SELECT uutisen_pvm as aika, Maakunta_ID, Teema, Uutinen, Hankkeen_luokitus, Url 
             FROM catbxjbt_ennakointi.Mediaseuranta
             WHERE Maakunta_ID = (SELECT maakunta_id FROM catbxjbt_ennakointi.Maakunnat WHERE maakunta LIKE '%" . $conn->real_escape_string($q) . "%')
             ORDER BY uutisen_pvm DESC
@@ -44,7 +44,8 @@ if ($result->num_rows > 0) {
         $cleanedUutinen = str_replace('-?', '-', $row["Uutinen"]);
 
         echo "<div class='record'>";
-        echo "" . $formattedDate . ", "; // Display the formatted date
+        echo $formattedDate . "   "; // Display the formatted date
+        echo $row["Hankkeen_luokitus"] . "  ";
    
         echo "<a href='" . $row["Url"] . "' target='_blank' class='styled-link'>" . $cleanedUutinen . "</a>, ";
         echo "</div><br>";
